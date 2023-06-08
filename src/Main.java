1109e4181c7053ece2ae057db8bf1911a59c20f9
chern <chernenkij,nikitacze@gmail.com>
2023-06-08 11:48:53 +0200

    Теоритическая часть курсовой работы Билет №3 - Задача Напишите программу, которая разделяет любую строку на две части ровно пополам по количеству символов.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача: Напишите программу, которая разделяет любую строку на две части ровно пополам по количеству символов. ");
+        System.out.println("Задача: Напишите программу, которая разделяет любую строку на две части ровно пополам по количеству символов.");
         String source = "Я обожаю изучать профессию Java-разрабочткик";
         String head = "";
         String tail = "";
-        System.out.println("Исходная строка: " + source);
+        System.out.println("Исходная строка:" + source);
 
-        // Проверим, не короткая ли строка, и дополним пробелами, если нужно
         while (source.length() < 2) {
             source = source + " ";
         }
@@ -18,8 +17,8 @@ public class Main {
         head = source.substring(0, source.length() / 2);
         tail = source.substring(source.length() / 2, source.length());
 
-        System.out.println("Первая половина: " + head);
-        System.out.println("Вторая половина: " + tail);
+        System.out.println("Первая половина:" + head);
+        System.out.println("Вторая половина:" + tail);
 
     }
 }
